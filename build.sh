98d12d41de0c9b27b0766f79b0d15bba90f61092
Max Neklyudov <macscomp@gmail.com>
2012-08-28 12:58:57 +0400

    Change toolset to arm-uclinuxeabi

--- build.sh
+++ build.sh
@@ -2,13 +2,13 @@
 
 
 ROOT=`pwd`
-TOOLCHAIN_BIN="$ROOT/../toolchain/arm-2011.03/bin"
+TOOLCHAIN_BIN="$ROOT/../toolchain/uclinux/arm-2011.03/bin"
 export PATH="$TOOLCHAIN_BIN:$PATH"
 
-make CROSS_COMPILE=arm-none-linux-gnueabi- clean
+make CROSS_COMPILE=arm-uclinuxeabi- clean
 
 make uwic_config || exit 1
-make -j 4 CROSS_COMPILE=arm-none-linux-gnueabi- ARCH=arm u-boot.bin || exit 1
+make -j 4 CROSS_COMPILE=arm-uclinuxeabi- ARCH=arm u-boot.bin || exit 1
 
 size $ROOT/u-boot
 
