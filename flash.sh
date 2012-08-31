29c1cf9f5e7c5cce1e17da87c3e3b3ba09648385
Max Neklyudov <macscomp@gmail.com>
2012-08-31 08:55:51 +0400

    Add local uwic-jtag.cfg

--- flash.sh
+++ flash.sh
@@ -2,6 +2,6 @@
 
 root=`pwd`
 image="$root/u-boot.bin"
-openocd -f "$root/../toolchain/jtag/uwic.cfg" -c "flash_mcu $image" || exit 1
+openocd -f "$root/board/elster/uwic/uwic-jtag.cfg" -c "flash_mcu $image" || exit 1
 
 echo "== Flashed =="
