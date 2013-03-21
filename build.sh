616efdc05cbf34e338b42e712740cf1777cb54f9
Max Neklyudov <macscomp@gmail.com>
2013-03-21 15:20:13 +0400

    Create composite bootloader image

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
-#!/bin/sh
-
+#!/bin/bash
 
 ROOT=`pwd`
 TOOLCHAIN_BIN="$ROOT/../toolchain/uclinux/arm-2011.03/bin"
@@ -16,6 +15,16 @@ mkfs.cramfs "$ROOT/rom_disk" "$ROOT/rom_disk.cramfs" || exit1
 
 echo "== Compiled =="
 
-if [ "$1" = "flash" ]; then
-  $ROOT/flash.sh
-fi
+image_size=$[512*1024]
+rom_disk_offset=327680
+bl_size=$(stat -c%s "$ROOT/u-boot.bin")
+rd_size=$(stat -c%s "$ROOT/rom_disk.cramfs")
+gap1=$[$rom_disk_offset - $bl_size]
+gap2=$[$image_size - $rom_disk_offset - $rd_size]
+
+cat u-boot.bin > image.bin
+tr "\000" "\377" < /dev/zero | dd ibs=1 count=$gap1 2> /dev/null >> image.bin
+cat rom_disk.cramfs >> image.bin
+tr "\000" "\377" < /dev/zero | dd ibs=1 count=$gap2 2> /dev/null >> image.bin
+
+echo "== Image ready =="
