7ff896498b8d86765533923155b1d8814b9a1e26
Max Neklyudov <macscomp@gmail.com>
2013-06-21 14:38:29 +0400

    Atlas: use wdt1 watchdog timer

--- arch/arm/cpu/cortex-m4/tm4c/watchdog.c
+++ arch/arm/cpu/cortex-m4/tm4c/watchdog.c
@@ -1,18 +1,37 @@
 #include <common.h>
 
-#define CURRENT_WDT 0
+#define CURRENT_WDT 1
 
-#define WDT_MAX_TIME     ((0xFFFFFFFF / CONFIG_SYSCLK_FREQUENCY) * 2) /* seconds */
+#if CURRENT_WDT == 0
+# define WATCHDOG_CLOCK_RATE CONFIG_SYSCLK_FREQUENCY
+#else
+# define WATCHDOG_CLOCK_RATE CONFIG_PIOSC_FREQUENCY
+#endif
+
+#define WDT_MAX_TIME     ((0xFFFFFFFF / WATCHDOG_CLOCK_RATE) * 2) /* seconds */
 
 #if CONFIG_WATCHDOG_TIMEOUT > WDT_MAX_TIME
 # error "CONFIG_WATCHDOG_TIMEOUT > WDT_MAX_TIME"
 #endif
 
+static inline void putregwdt32(uint32_t value, uint32_t addr)
+{
+	putreg32(value, addr);
+#if CURRENT_WDT == 1
+	while( (getreg32(LM3S_WATCHDOG_WDTCTL(CURRENT_WDT)) & WATCHDOG_WDTCTL_WRC_MASK) == 0 ) {}
+#endif
+}
+
+static inline uint32_t getregwdt32(uint32_t addr)
+{
+	return getreg32(addr);
+}
+
 static int wd_ready = 0;
 
 static inline void _wdt_unlock(void)
 {
-	putreg32(WATCHDOG_WDTLOCK_MAGIC, LM3S_WATCHDOG_WDTLOCK(CURRENT_WDT));
+	putregwdt32(WATCHDOG_WDTLOCK_MAGIC, LM3S_WATCHDOG_WDTLOCK(CURRENT_WDT));
 }
 
 /*
@@ -20,7 +39,7 @@ static inline void _wdt_unlock(void)
  */
 static inline void lm3s_wdt_reload(void)
 {
-	putreg32(1, LM3S_WATCHDOG_WDTICR(CURRENT_WDT));
+	putregwdt32(1, LM3S_WATCHDOG_WDTICR(CURRENT_WDT));
 }
 
 /*
@@ -29,20 +48,20 @@ static inline void lm3s_wdt_reload(void)
 static inline void lm3s_wdt_start(void)
 {
 	uint32_t regval;
-	uint32_t tval = CONFIG_WATCHDOG_TIMEOUT * (CONFIG_SYSCLK_FREQUENCY / 2);
+	uint32_t tval = CONFIG_WATCHDOG_TIMEOUT * (WATCHDOG_CLOCK_RATE / 2);
 
 	watchdog_clock_ctrl(CURRENT_WDT, SYS_ENABLE_CLOCK);
 
 	_wdt_unlock();
 
-	putreg32(tval, LM3S_WATCHDOG_WDTLOAD(CURRENT_WDT));
+	putregwdt32(tval, LM3S_WATCHDOG_WDTLOAD(CURRENT_WDT));
 	lm3s_wdt_reload();
 
-	regval = getreg32(LM3S_WATCHDOG_WDTCTL(CURRENT_WDT));
+	regval = getregwdt32(LM3S_WATCHDOG_WDTCTL(CURRENT_WDT));
 	regval |= WATCHDOG_WDTCTL_RESEN_MASK;
-	putreg32(regval, LM3S_WATCHDOG_WDTCTL(CURRENT_WDT));
+	putregwdt32(regval, LM3S_WATCHDOG_WDTCTL(CURRENT_WDT));
 	regval |= WATCHDOG_WDTCTL_INTEN_MASK;
-	putreg32(regval, LM3S_WATCHDOG_WDTCTL(CURRENT_WDT));
+	putregwdt32(regval, LM3S_WATCHDOG_WDTCTL(CURRENT_WDT));
 
 	wd_ready = 1;
 }
